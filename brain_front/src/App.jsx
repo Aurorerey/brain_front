9e07897bfa75e52c2d1724be052ae4779fc6aeaf
Aurore <aurore@lica-europe.org>
2023-09-07 12:21:27 +0200

    adding structure

--- brain_front/src/App.jsx
+++ brain_front/src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 import { useState } from 'react'
+import Game from ".components/Game"
+import Login from ".components/Login"
+import Rank from ".components/Rank"
+import Team from ".components/Team"
 import './App.css'
 
 
---
+++ brain_front/src/components/rank.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Rank() {
+    return (
+        <div>
+            <h1>This is the rank page</h1>
+        </div>
+    )
+}
+
+export default Rank
